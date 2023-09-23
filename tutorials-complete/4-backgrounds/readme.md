874f8905e6088b91f556ef0dbe7676f1b54216d7
Rein van der Woerd <reinvanderwoerd@me.com>
2023-09-23 11:27:07 +0200

    Before workshop

--- tutorials-complete/4-backgrounds/readme.md
+++ tutorials-complete/4-backgrounds/readme.md
@@ -2,3 +2,6 @@
 
 Learn how to use background layers and draw a background image.
 - And scroll backgrounds
+
+Using 3 tiling background layers, for the sky and grass and trees!!!
+
--- tutorials-complete/6-arrays/source/main.cpp
+++ tutorials-complete/6-arrays/source/main.cpp
@@ -1,7 +1,8 @@
 #include <nds.h> 
 #include <stdio.h>
-#include <nf_lib.h> // Include NFlib, which we will use to work with sprites
-#include <filesystem.h> // Include filesystem, needed to load files from NitroFS
+#include <nf_lib.h> 
+#include <filesystem.h>
+#include <vector>
 
 
 // We will use this to indentify the palette of the sprite and the sprite itself
@@ -9,10 +10,23 @@ const int PALLETTE_ID = 0;
 const int SPRITE_RAM_SLOT = 0;
 
 
+struct Fire {
+    int x;
+    int y;
+};
+
+// The vector will contain all the fires on the screen
+const auto fires = std::vector<Fire>();
+
+
+
+
 int main()
 {
     // Initialize 2D engine in the bottom screen
     NF_Set2D(1, 0);
+
+    // The following code is the same as in the previous tutorials ------------
 
     // Initialize NitroFS and set it as the root folder of the filesystem
     nitroFSInit(nullptr);
@@ -25,25 +39,24 @@ int main()
 
 
     // Load the sprite's palette from filesystem to RAM...
-    NF_LoadSpritePal("sprite", PALLETTE_ID);
+    NF_LoadSpritePal("fire", PALLETTE_ID);
     // ... and transfer it to VRAM of the bottom screen.
     NF_VramSpritePal(1, PALLETTE_ID, PALLETTE_ID);
     // If you want to use the same palette for multiple sprites, you only need to do this once.
 
-
     // Load the sprite's graphics from filesystem to RAM...
-    NF_LoadSpriteGfx("sprite", 0, 32, 32);
+    NF_LoadSpriteGfx("fire", 0, 32, 32);
     // ... and transfer it to VRAM of the bottom screen
     NF_VramSpriteGfx(1, 0, 0, true);
 
 
-    // Set the sprite's initial position to the center of the screen
-    int x = SCREEN_WIDTH / 2 - 16;
-    int y = SCREEN_HEIGHT / 2 - 16;
+    // --------------------------------
 
+
+    
 
     // Finally create the sprite on the center of the screen
-    NF_CreateSprite(1, 0, 0, PALLETTE_ID, x, y);
+    // NF_CreateSprite(1, 0, 0, PALLETTE_ID, x, y);
 
 
     
@@ -53,8 +66,6 @@ int main()
         touchPosition touch;
         touchRead(&touch);
 
-        // Print the touch position
-        printf("\x1b[1;1HTouch: %04i, %04i", touch.px, touch.py);
         
         // If the user touched the screen...
         if (touch.px != 0 && touch.py != 0)  {
